ef32da9ab600de84d7f6d55bc5846332689de0fa
Sergio Sosa <sergioarsosa95@gmail.com>
2025-08-13 01:39:20 -0600

    Lazyload exercise done

---
+++ src/routes/routes.ts
@@ -0,0 +1,34 @@
+import { lazy, type JSX, type LazyExoticComponent } from 'react';
+import { NoLazy } from '../01-lazyload/pages/NoLazy';
+// import { LazyPage1, LazyPage2, LazyPage3 } from '../01-lazyload/pages';
+
+type JSXComponent = () => JSX.Element;
+
+interface Route {
+  to: string;
+  path: string;
+  Component: LazyExoticComponent<JSXComponent> | JSXComponent;
+  name: string;
+}
+
+const lazyLayout = lazy(
+  () =>
+    import(
+      /* webpackChunkName: 'LazyLayout' */ '../01-lazyload/layout/LazyLayout'
+    )
+);
+
+export const routes: Route[] = [
+  {
+    to: '/lazyload/',
+    path: '/lazyload/*',
+    Component: lazyLayout,
+    name: 'Lazy-load',
+  },
+  {
+    to: '/no-lazy',
+    path: 'no-lazy',
+    Component: NoLazy,
+    name: 'NoLazy',
+  },
+];
